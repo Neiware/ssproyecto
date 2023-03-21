13ae82abd9d13e6eb3ab53b25f2fe95b269ca116
Neiware <102248010+Neiware@users.noreply.github.com>
2023-03-20 20:44:25 -0600

    setup macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <Arduino.h>
-#include "AlgLib.h"
+
+//----------------MACROS----------------------//
+
+//---Outputs for Mux---//
+#define MUX_A 2 
+#define MUX_B 3
+#define MUX_C 4
+#define MUX_D 5
+//---Input for Mux---//
+#define MUX_Y 6
+//---Modulo SD-------//
+#define SD_SCK 13
+#define SD_MISO 12
+#define SD_MOSI 11
+#define SD_CS 10
+//---Modulo Bocina---//
+#define BOCINA_PWM 9
+//---Modulo I2C------//
+#define I2C_SDA A4
+#define I2C_SCL A5
+
 
 void setup() {
-  // put your setup code here, to run once:
-  char str[10];
-  int mano;
-   SeleCombo(1,str);
-  printf("se, %s", str);
-  scanf("bith %d", &mano);
+  // SETUP I/O FOR MUX
+  //OUTPUTS 
+
+
 //new commento to test branch master
 
 }
 
-void loop() {
-
-  
-
- 
+void loop() { 
   // put your main code here, to run repeatedly:
 }
