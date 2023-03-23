8e260701695359b7639ec177f2ed8622d9ece536
Neiware <102248010+Neiware@users.noreply.github.com>
2023-03-23 16:04:23 -0600

    setup I/O, add I2C-LCD object, object for lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <LiquidCrystal_I2C.h> //library for communication I2C with LCD
 //----------------MACROS----------------------//
 
 //---Outputs for Mux---//
@@ -20,10 +20,23 @@
 #define I2C_SDA A4
 #define I2C_SCL A5
 
+//----------CREATE OBJEXTS------//
+//OBJECT CALL lcd using external library
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup() {
   // SETUP I/O FOR MUX
-  //OUTPUTS 
+  pinMode(MUX_A,OUTPUT);
+  pinMode(MUX_B,OUTPUT);
+  pinMode(MUX_C,OUTPUT);
+  pinMode(MUX_D,OUTPUT);
+  pinMode(MUX_Y,INPUT);
+  //inicitialize low
+  digitalWrite(MUX_A,LOW);
+  digitalWrite(MUX_B,LOW);
+  digitalWrite(MUX_C,LOW);
+  digitalWrite(MUX_D,LOW);
+  // SETUP I2C FOR LCD WITH LIBRARY INCLUDED
 
 
 //new commento to test branch master
