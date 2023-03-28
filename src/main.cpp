851027ba99bdaa9fa6cd74c7b6668e3aff88476a
Neiware <102248010+Neiware@users.noreply.github.com>
2023-03-27 20:28:39 -0600

    Ready to Test SD-Test

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 //---Input for Mux---//
 #define MUX_Y 6
 //---Modulo SD-------//
+#define SD_ChipSelecPin 8 // Pin 8 is gonna be CS for the SD Module 
 #define SD_SCK 13
 #define SD_MISO 12
 #define SD_MOSI 11
@@ -24,6 +25,7 @@
 //----------CREATE OBJEXTS------//
 //OBJECT CALL lcd using external library
 LiquidCrystal_I2C lcd(0x27,20,4);
+TMRpcm Audio; //object for TMRpcm library
 
 void setup() {
   // SETUP I/O FOR MUX
@@ -39,7 +41,13 @@ void setup() {
   digitalWrite(MUX_D,LOW);
   // SETUP I2C FOR LCD WITH LIBRARY INCLUDED
 
-
+  //SETUP SPEAKER
+  Audio.speakerPin = BOCINA_PWM; //select pin connected from arduino to speaker 
+  /* OPTIONAL SETTINGS
+  Audio.quality(1); // Improve quality of the sound, but i think will consume more power and time for instruction
+  Audio.setVolume(5); // Select volume from 0 to 7
+  */
+ 
 //new commento to test branch master
 
 }
