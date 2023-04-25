427a76518408c5d1a5d60c57917576dc404280d0
Neiware <102248010+Neiware@users.noreply.github.com>
2023-04-25 15:45:02 -0600

    big changes, first test for speaker and SD. Worked

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,49 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h> //library for communication I2C with LCD
 #include <TMRpcm.h> // library for SD and Speaker communicatios.
-//----------------MACROS----------------------//
+#include <SPI.h>
+#include <SD.h>
 
+//----------------MACROS----------------------//
+/*
 //---Outputs for Mux---//
 #define MUX_A 2 
 #define MUX_B 3
 #define MUX_C 4
 #define MUX_D 5
 //---Input for Mux---//
-#define MUX_Y 6
+#define MUX_Y 6*/
 //---Modulo SD-------//
-#define SD_ChipSelecPin 8 // Pin 8 is gonna be CS for the SD Module 
+#define SD_ChipSelecPin 10 // Pin 10 is gonna be CS for the SD Module 
 #define SD_SCK 13
 #define SD_MISO 12
 #define SD_MOSI 11
-#define SD_CS 10
 //---Modulo Bocina---//
 #define BOCINA_PWM 9
+/*
 //---Modulo I2C------//
 #define I2C_SDA A4
-#define I2C_SCL A5
+#define I2C_SCL A5*/
 
 //----------CREATE OBJEXTS------//
 //OBJECT CALL lcd using external library
-LiquidCrystal_I2C lcd(0x27,20,4);
+//LiquidCrystal_I2C lcd(0x27,20,4);
 TMRpcm Audio; //object for TMRpcm library
 
 void setup() {
-  // SETUP I/O FOR MUX
+  Serial.begin(9600);
+   //SETUP SD 
+  if (SD.begin(SD_ChipSelecPin)) //check if SD.begin is working. .being return boolean
+  {
+    Serial.println("Working");
+    //YES IS WORKING 
+    //Maybe add sound or show response in the LCD
+  }else{
+    Serial.println("not Working");
+    //NOT WORKING 
+    //Display Eror to the user and reset the whole program
+  }
+  /*// SETUP I/O FOR MUX
   pinMode(MUX_A,OUTPUT);
   pinMode(MUX_B,OUTPUT);
   pinMode(MUX_C,OUTPUT);
@@ -38,32 +53,24 @@ void setup() {
   digitalWrite(MUX_A,LOW);
   digitalWrite(MUX_B,LOW);
   digitalWrite(MUX_C,LOW);
-  digitalWrite(MUX_D,LOW);
+  digitalWrite(MUX_D,LOW);*/
   // SETUP I2C FOR LCD WITH LIBRARY INCLUDED
 
   //SETUP SPEAKER
   Audio.speakerPin = BOCINA_PWM; //select pin connected from arduino to speaker 
-  /* OPTIONAL SETTINGS
+
   Audio.quality(1); // Improve quality of the sound, but i think will consume more power and time for instruction
   Audio.setVolume(5); // Select volume from 0 to 7
-  */
-  //SETUP SD 
-  if (SD.begin(SD_ChipSelecPin)) //check if SD.begin is working. .being return boolean
-  {
-    //YES IS WORKING 
-    //Maybe add sound or show response in the LCD
-  }else{
-    //NOT WORKING 
-    //Display Eror to the user and reset the whole program
-  }
+  
+ 
   
 
   //------------------TEST SD ADN SPEAKER ONLY------------------//
-  //Audio.play("Name of the file.wav"); Function is to play the file selecting a file
+  Audio.play("Toni.wav"); //Function is to play the file selecting a file
                                       // I still need to get the SD and add files so
                                       //not ready yet
-  delay(3000);
-  Audio.pause();// Method to pause the audio, the delay is to check how this methods works during runtime
+
+  //Audio.pause();// Method to pause the audio, the delay is to check how this methods works during runtime
 //new commento to test branch master
 
 }
